65c4fa73ae32a8ec981179d408b77cf1565b9381
Israel Pérez Saucedo <neomatrixisra25@hotmail.com>
2020-10-22 22:31:40 -0500

    Add files via upload

---
+++ DemoColores48/DemoColores48/Diccionarios/DiccionarioColores.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoColores48.Diccionarios
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DiccionarioColores : ResourceDictionary
+    {
+        public DiccionarioColores()
+        {
+            InitializeComponent();
+        }
+    }
+}
